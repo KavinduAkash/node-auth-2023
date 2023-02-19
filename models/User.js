8640f4f84f8c9e661bcc2bf8f5140b41b6e4310f
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-02-19 10:25:35 +0530

    hash the password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -9,6 +10,12 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+})
+
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 const User = mongoose.model('user', userSchema)
